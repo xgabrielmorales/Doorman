974e2af581f181dbaa86f33f9eababd8d33af2ce
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-09-19 17:47:03 -0500

    test: base tests

---
+++ tests/conftest.py
@@ -0,0 +1,86 @@
+import asyncio
+import os
+
+import pytest
+import pytest_asyncio
+from fastapi.testclient import TestClient
+from sqlalchemy import event
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+from src.core.settings import settings
+
+async_engine = create_async_engine(
+    settings.POSTGRES_URL,
+    pool_size=5,
+    echo=True,
+    max_overflow=10,
+)
+
+TestingAsyncSessionLocal = sessionmaker(
+    async_engine,
+    expire_on_commit=False,
+    autoflush=False,
+    autocommit=False,
+    class_=AsyncSession,
+)
+
+
+@pytest_asyncio.fixture(scope="function")
+async def async_db_session():
+    connection = await async_engine.connect()
+    trans = await connection.begin()
+    async_session = TestingAsyncSessionLocal(bind=connection)
+    nested = await connection.begin_nested()
+
+    @event.listens_for(async_session.sync_session, "after_transaction_end")
+    def end_savepoint(session, transaction):
+        nonlocal nested
+
+        if not nested.is_active:
+            nested = connection.sync_connection.begin_nested()
+
+    yield async_session
+
+    await trans.rollback()
+    await async_session.close()
+    await connection.close()
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    policy = asyncio.get_event_loop_policy()
+    loop = policy.new_event_loop()
+
+    yield loop
+
+    loop.close()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def verify_env_variables():
+    required_env_variables = [
+        "SECRET_KEY",
+        "POSTGRES_USER",
+        "POSTGRES_HOST",
+        "POSTGRES_PASSWORD",
+        "POSTGRES_DB",
+    ]
+
+    missing_env_variables: list[str] = []
+    for var in required_env_variables:
+        if not os.getenv(var):
+            missing_env_variables.append(var)
+
+    if not missing_env_variables:
+        return
+
+    missing_env_variables_str = ", ".join(missing_env_variables)
+    pytest.fail(f"Required environment variables are missing: {missing_env_variables_str}")
+
+
+@pytest.fixture(scope="session")
+def client():
+    from src.main import app
+
+    return TestClient(app)
---
+++ tests/services/test_create_user.py
@@ -0,0 +1,78 @@
+import pytest
+from fastapi import HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.future import select
+
+from src.core.models.user import User
+from src.core.schemas.auth import CreateUser
+from src.services.users import create_user, user_login
+
+
+@pytest.fixture
+def user_data():
+    return CreateUser(
+        name="Test User",
+        username="testuser",
+        password="password123",
+    )
+
+
+class TestCreateUser:
+    @pytest.mark.asyncio
+    async def test_create_user_new_username(self, async_db_session, user_data):
+        user = await create_user(db=async_db_session, user_data=user_data)
+
+        query = select(User).where(User.username == user_data.username)
+        result = await async_db_session.execute(query)
+        created_user = result.scalars().first()
+
+        assert user.username == user_data.username
+        assert user.name == user_data.name
+
+        assert created_user is not None
+
+    @pytest.mark.asyncio
+    async def test_create_user_existing_username(self, async_db_session, user_data):
+        await create_user(db=async_db_session, user_data=user_data)
+
+        with pytest.raises(HTTPException) as exc_info:
+            await create_user(db=async_db_session, user_data=user_data)
+
+        assert exc_info.value.status_code == status.HTTP_400_BAD_REQUEST
+        assert "User with the same username or document number already exists" in str(exc_info.value)
+
+    @pytest.mark.asyncio
+    async def test_create_user_password_hashing(self, async_db_session, user_data):
+        user = await create_user(db=async_db_session, user_data=user_data)
+
+        assert user.password != user_data.password
+
+
+class TestUserLogin:
+    @pytest.mark.asyncio
+    async def test_user_login_valid_credentials(self, async_db_session, user_data):
+        await create_user(db=async_db_session, user_data=user_data)
+
+        auth_data = OAuth2PasswordRequestForm(
+            username=user_data.username,
+            password=user_data.password,
+        )
+
+        token = await user_login(async_db_session, auth_data)
+
+        assert isinstance(token, str)
+
+    @pytest.mark.asyncio
+    async def test_user_login_invalid_credentials(self, async_db_session, user_data):
+        await create_user(db=async_db_session, user_data=user_data)
+
+        auth_data = OAuth2PasswordRequestForm(
+            username=user_data.username,
+            password="Wrong password",
+        )
+
+        with pytest.raises(HTTPException) as exc_info:
+            await user_login(db=async_db_session, auth_data=auth_data)
+
+        assert exc_info.value.status_code == 401
+        assert exc_info.value.detail == "Invalid username or password"
