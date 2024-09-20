a2dae18f6d48f44d2119b899502e2f081e4258a8
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-09-20 09:50:48 -0500

    build: better approach to a build

--- src/core/settings.py
+++ src/core/settings.py
@@ -8,7 +8,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
 
-    SECRET_KEY: str
+    # SECURITY WARNING: keep the secret key used in production secret!
+    SECRET_KEY: str = "3RWM3zT68QEaOacQiYmSVzNyOHnJMpqVQi8mS2zN"
 
     POSTGRES_HOST: str
     POSTGRES_PASSWORD: str
