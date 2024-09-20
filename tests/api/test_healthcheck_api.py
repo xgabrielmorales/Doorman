3b367f18fde0dd4cb42bb7193a13665025e58a5b
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-09-20 09:48:35 -0500

    test: fix healthcheck test

--- tests/api/test_healthcheck_api.py
+++ tests/api/test_healthcheck_api.py
@@ -1,8 +1,13 @@
 import pytest
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
 
 
 @pytest.mark.asyncio
-def test_healthcheck(client):
+def test_healthcheck():
     response = client.get("/healthcheck")
     assert response.status_code == 200
-    assert response.json() == {"PostgreSQL": "ok"}
+    assert response.json() == {"PostgreSQL": "healthy"}
