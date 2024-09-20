9c65bb561037855ae728da13cd37bd139628a941
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-09-19 21:54:53 -0500

    test: expired token

--- tests/services/test_create_user.py
+++ tests/services/test_create_user.py
@@ -1,12 +1,15 @@
+from datetime import datetime, timedelta
+
 import pytest
 from fastapi import HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
+from freezegun import freeze_time
+from pydantic import BaseModel
 from sqlalchemy.future import select
 
 from src.core.models.user import User
 from src.core.schemas.auth import CreateUser
-from src.services.users import create_user, user_login, get_current_user
-from pydantic import BaseModel
+from src.services.users import create_user, get_current_user, user_login
 
 
 @pytest.fixture
@@ -40,7 +43,9 @@ class TestCreateUser:
             await create_user(db=async_db_session, user_data=user_data)
 
         assert exc_info.value.status_code == status.HTTP_400_BAD_REQUEST
-        assert "User with the same username or document number already exists" in str(exc_info.value)
+        assert "User with the same username or document number already exists" in str(
+            exc_info.value
+        )
 
     @pytest.mark.asyncio
     async def test_create_user_password_hashing(self, async_db_session, user_data):
@@ -80,7 +85,7 @@ class TestUserLogin:
         assert exc_info.value.detail == "Invalid username or password"
 
     @pytest.mark.asyncio
-    async def test_user_get_session(self, async_db_session, user_data):
+    async def test_user_get_session_data(self, async_db_session, user_data):
         user = await create_user(db=async_db_session, user_data=user_data)
 
         auth_data = OAuth2PasswordRequestForm(
@@ -97,3 +102,27 @@ class TestUserLogin:
 
         assert user.id == authenticated_user.id
         assert user.username == authenticated_user.username
+
+    @pytest.mark.asyncio
+    async def test_user_invalid_token(self, async_db_session, user_data):
+        invalid_access_token = "eyJhbGciOiJIpXVCJ9.eyJzdWjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwV_adQssw5c"
+
+        with pytest.raises(HTTPException, match="401: Invalid token"):
+            await get_current_user(access_token=invalid_access_token, db=async_db_session)
+
+    @pytest.mark.asyncio
+    async def test_user_modified_token(self, async_db_session, user_data):
+        await create_user(db=async_db_session, user_data=user_data)
+
+        auth_data = OAuth2PasswordRequestForm(
+            username=user_data.username,
+            password=user_data.password,
+        )
+
+        jwt_payload = await user_login(async_db_session, auth_data)
+        PLUS_30_MINUTES = datetime.now() + timedelta(minutes=30, seconds=1)
+
+        with freeze_time(PLUS_30_MINUTES), pytest.raises(
+            HTTPException, match="401: Signature has expired"
+        ):
+            await get_current_user(access_token=jwt_payload.access_token, db=async_db_session)
