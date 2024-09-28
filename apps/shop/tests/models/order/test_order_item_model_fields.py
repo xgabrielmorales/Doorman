01f93650f2a21b85b620aa84e175fbe5c89139b9
Gabriel Morales <gabrielmorales1@protonmail.com>
2024-09-28 15:05:40 -0500

    test(order): add tests for the orders model

---
+++ apps/shop/tests/models/order/test_order_item_model_fields.py
@@ -0,0 +1,28 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+
+from shop.order.models import OrderItem
+from tests.models.common import BaseModelFieldTest
+
+
+class BaseTestOrderItem(BaseModelFieldTest):
+    model = OrderItem
+
+
+class TestFieldOrderItemItem(BaseTestOrderItem):
+    field_name = "item"
+    field_type = models.ForeignKey
+    db_index = True
+
+
+class TestFieldOrderItemOrder(BaseTestOrderItem):
+    field_name = "order"
+    field_type = models.ForeignKey
+    db_index = True
+
+
+class TestFieldOrderItemQuantity(BaseTestOrderItem):
+    field_name = "quantity"
+    field_type = models.IntegerField
+    validators = (MinValueValidator,)
+    default = 1
---
+++ apps/shop/tests/models/order/test_order_model_fields.py
@@ -0,0 +1,26 @@
+from django.core.validators import MinValueValidator
+from django.db import models
+from shop.order.models import Order
+
+from tests.models.common import BaseModelFieldTest
+
+
+class BaseTestOrder(BaseModelFieldTest):
+    model = Order
+
+
+class TestFieldOrderGrossCost(BaseTestOrder):
+    field_name = "gross_cost"
+    field_type = models.DecimalField
+    validators = (MinValueValidator,)
+
+
+class TestFieldOrderNetCost(BaseTestOrder):
+    field_name = "net_cost"
+    field_type = models.DecimalField
+    validators = (MinValueValidator,)
+
+
+class TestFieldOrderItems(BaseTestOrder):
+    field_name = "items"
+    field_type = models.ManyToManyField
